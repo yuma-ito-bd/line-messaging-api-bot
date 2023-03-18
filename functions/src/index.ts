fc631c8388dff95fa23309ce0fe0d1fc7f1b4848
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2023-03-18 13:58:09 +0900

    Cloud Functionsの環境構成のパラメータを利用するように修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,21 @@
 import {https, logger} from "firebase-functions";
+import {defineString} from "firebase-functions/params";
 import {WebhookRequestBody, Client} from "@line/bot-sdk";
 
-const lineClient = new Client({
-  channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN ?? "invalid token",
-});
+// 実行時に必要なパラメータを定義
+const config = {
+  channelSecret: defineString("CHANNEL_SECRET"),
+  channelAccessToken: defineString("CHANNEL_ACCESS_TOKEN"),
+};
 
 export const webhook = https.onRequest((req, res) => {
   res.send("HTTP POST request sent to the webhook URL!");
+
+  // LINE Messaging API Clientの初期化
+  const lineClient = new Client({
+    channelSecret: config.channelSecret.value(),
+    channelAccessToken: config.channelAccessToken.value(),
+  });
 
   // ユーザーがbotに送ったメッセージをそのまま返す
   const {events} = req.body as WebhookRequestBody;
